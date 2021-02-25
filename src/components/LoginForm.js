d7d83a3790cd82a7a4de1c5d148645956fbdd120
Shilcof <robert.shilcof@gmail.com>
2021-02-25 13:37:34 +0000

    commit tree 6cb09aa779a0343315406a0aeff8fc021b9bcf07

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -10,9 +10,16 @@ class LoginForm extends React.Component {
     })
   }
 
+  handleSubmit = e => {
+    e.preventDefault()
+    if (this.state.username && this.state.password) {
+      this.props.handleLogin(this.state)
+    }
+  }
+
   render() {
     return (
-      <form onSubmit={this.props.handleLogin}>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
